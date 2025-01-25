467cf597e0e974566375fd29debcf671511c433c
Goooooooooooo1212 <goooooooooooo1212@outlook.es>
2025-01-25 16:19:04 +0100

    Publicados creditos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,17 +8,28 @@ public class GameManager : MonoBehaviour
     AudioSource auso;
     public GameObject fadein;
     public AudioClip click;
+    public GameObject panelcreditos;
+    public static bool cr;
+    bool cract;
     // Start is called before the first frame update
     void Start()
     {
         auso = GetComponent<AudioSource>();
         Invoke("fade", 1f);
+        panelcreditos.SetActive(false);
+        cr = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C) && cr == false && cract == true && Creditos.tact == true)
+        {
+            cr = true;
+            Invoke("outcredit", 1f);
+            cract = false;
 
+        }
     }
     public void Play ()
     {
@@ -35,12 +46,20 @@ public class GameManager : MonoBehaviour
         Application.Quit();
 
     }
-    public void Onclick()
-    {
-
-    }
     public void fade()
     {
         fadein.SetActive (false);
+    }
+    public void creditos()
+    { 
+        panelcreditos.SetActive (true);
+        auso.PlayOneShot(click);
+        cract = true;
+        
+    }
+    public void outcredit()
+    {
+        cr = false;
+        panelcreditos.SetActive(false);
     }
 }
